f656a4730fc05558021d9982ee281a4aac58b1b6
Henry Ainouze <you@example.com>
2023-08-28 16:46:14 +0300

    try

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import morgan from 'morgan';
 import router from './api/products/routes.product.js';
 import usersRouter from './api/users/routes.users.js';
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(morgan('dev')); 
 
 app.use('/api/products',router)
