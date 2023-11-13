587ffb7320e4c4e3cc922b06573f5d9b04057ccf
yishayainouze <17iyar@gmail.com>
2023-11-13 15:04:31 +0200

    Yishay

--- app.ts
+++ app.ts
@@ -1,17 +1,19 @@
-import express, { Request, Response, NextFunction } from 'express';
+// app.ts
+import express from 'express';
 import cors, { CorsOptions } from 'cors';
 import morgan from 'morgan';
-import router from './api/products/products.Routes';
-import  usersRouter  from './api/users/routes.users';
-
 import mongoose from 'mongoose';
+import router from './api/products/products.Routes';
+import usersRouter from './api/users/routes.users';
+import categoryRouter from './api/categories/category.routes'; 
 
 const app: express.Application = express();
 const PORT: number = 8080;
-mongoose.connect('mongodb+srv://team-alpha:1234@cluster0.orqsdi2.mongodb.net/project-data')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
 
+mongoose
+  .connect('mongodb+srv://team-alpha:1234@cluster0.orqsdi2.mongodb.net/project-data')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -29,6 +31,7 @@ app.use(morgan('dev'));
 
 app.use('/api/products', router);
 app.use('/api/users', usersRouter);
+app.use('/api/categories', categoryRouter); 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
